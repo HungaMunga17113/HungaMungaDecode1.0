0c1b255d84742ae50f41a88af6e3f2338eb3c9ae
ishaandesai <IshaanDesai21@gmail.com>
2025-09-22 12:05:28 -0500

    OOP Drive Functions

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Encoder_Drive/EncoderAutonTemplate.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Encoder_Drive/EncoderAutonTemplate.java
@@ -10,25 +10,20 @@ public class EncoderAutonTemplate extends LinearOpMode {
     //Ticks multiplied by inches in same distance
     private double ticksPerIn = 45;
 
-    //Declare Motors
-    private DcMotorEx leftFront;
-    private DcMotorEx rightFront;
-    private DcMotorEx leftBack;
-    private DcMotorEx rightBack;
-
-    private DcMotorEx intake;
+    DriveFunctions df = new DriveFunctions();
 
     @Override
     public void runOpMode() {
 
         //---------------- INITIALIZATION ----------------\\
         //Drive Motor Config
-        leftFront = hardwareMap.get(DcMotorEx.class, "leftFront");
-        rightFront = hardwareMap.get(DcMotorEx.class, "rightFront");
-        leftBack = hardwareMap.get(DcMotorEx.class, "leftBack");
-        rightBack = hardwareMap.get(DcMotorEx.class, "rightBack");
+        //Declare Motors
+        DcMotorEx leftFront = hardwareMap.get(DcMotorEx.class, "leftFront");
+        DcMotorEx rightFront = hardwareMap.get(DcMotorEx.class, "rightFront");
+        DcMotorEx leftBack = hardwareMap.get(DcMotorEx.class, "leftBack");
+        DcMotorEx rightBack = hardwareMap.get(DcMotorEx.class, "rightBack");
 
-        intake = hardwareMap.get(DcMotorEx.class, "intake");
+        DcMotorEx intake = hardwareMap.get(DcMotorEx.class, "intake");
 
 
         //Resets all encoder values to 0
@@ -71,141 +66,11 @@ public class EncoderAutonTemplate extends LinearOpMode {
         //**NOTE - NEVER set power to 1.0 at the start, start at 0.5 and work your way up.
         // 1.0 loses lots of consistency, and the robot movements would be choppy
 
+        df.vertical(500,0.5,5);
 
 
     }
 
-    //Positive value will go forward, Negative value will go backward
-    public void vertical(int ticks, double speed, long seconds) {
-        reset();
 
-        leftFront.setTargetPosition(ticks);
-        leftBack.setTargetPosition(ticks);
-        rightFront.setTargetPosition(ticks);
-        rightBack.setTargetPosition(ticks);
-
-        leftFront.setVelocity(speed);
-        leftBack.setVelocity(speed);
-        rightFront.setVelocity(speed);
-        rightBack.setVelocity(speed);
-
-        sleep(seconds * 1000);
-    }
-
-    public void verticalInInches(double inches, double speed, long seconds) {
-        reset();
-        int ticks = toTicks(inches);
-
-        leftFront.setTargetPosition(ticks);
-        leftBack.setTargetPosition(ticks);
-        rightFront.setTargetPosition(ticks);
-        rightBack.setTargetPosition(ticks);
-
-        leftFront.setVelocity(speed);
-        leftBack.setVelocity(speed);
-        rightFront.setVelocity(speed);
-        rightBack.setVelocity(speed);
-
-        sleep(seconds * 1000);
-    }
-
-    //Positive value will strafe right, Negative value will strafe left
-    public void strafe(int ticks, double speed, long seconds) {
-        reset();
-
-        leftFront.setTargetPosition(ticks);
-        leftBack.setTargetPosition(-ticks);
-        rightFront.setTargetPosition(-ticks);
-        rightBack.setTargetPosition(ticks);
-
-        leftFront.setVelocity(speed);
-        leftBack.setVelocity(speed);
-        rightFront.setVelocity(speed);
-        rightBack.setVelocity(speed);
-
-        sleep(seconds * 1000);
-    }
-
-    public void strafeInInches(double inches, double speed, long seconds) {
-        reset();
-        int ticks = toTicks(inches);
-
-        leftFront.setTargetPosition(ticks);
-        leftBack.setTargetPosition(-ticks);
-        rightFront.setTargetPosition(-ticks);
-        rightBack.setTargetPosition(ticks);
-
-        leftFront.setVelocity(speed);
-        leftBack.setVelocity(speed);
-        rightFront.setVelocity(speed);
-        rightBack.setVelocity(speed);
-
-        sleep(seconds * 1000);
-    }
-    //Positive value will turn right, Negative value will turn left
-    public void turn(int ticks, double speed, long seconds) {
-        reset();
-
-        leftFront.setTargetPosition(-ticks);
-        leftBack.setTargetPosition(-ticks);
-        rightFront.setTargetPosition(ticks);
-        rightBack.setTargetPosition(ticks);
-
-        leftFront.setVelocity(speed);
-        leftBack.setVelocity(speed);
-        rightFront.setVelocity(speed);
-        rightBack.setVelocity(speed);
-
-        sleep(seconds * 1000);
-    }
-
-    public void turnInInches(double inches, double speed, long seconds) {
-        reset();
-        int ticks = toTicks(inches);
-
-        leftFront.setTargetPosition(-ticks);
-        leftBack.setTargetPosition(-ticks);
-        rightFront.setTargetPosition(ticks);
-        rightBack.setTargetPosition(ticks);
-
-        leftFront.setVelocity(speed);
-        leftBack.setVelocity(speed);
-        rightFront.setVelocity(speed);
-        rightBack.setVelocity(speed);
-
-        sleep(seconds * 1000);
-    }
-    public void intake(double speed, long time) {
-        intake.setVelocity(speed);
-        sleep(time);
-        intake.setVelocity(0);
-    }
-
-    //Converts ticks into inches
-    //Input: inches
-    //Output/return: ticks
-    public int toTicks(double inches) {
-        double ticks = inches * 45;
-        return (int) ticks;
-    }
-    public void reset() {
-        //Resets all encoder values to 0
-        leftFront.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
-        rightFront.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
-        leftBack.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
-        rightBack.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
-
-        //Sets target position to 0
-        leftFront.setTargetPosition(0);
-        rightFront.setTargetPosition(0);
-        leftBack.setTargetPosition(0);
-        rightBack.setTargetPosition(0);
-
-        //Sets motor mode to Run to Position to make sure no motor has a target position set (0)
-        leftFront.setMode(DcMotorEx.RunMode.RUN_TO_POSITION);
-        rightFront.setMode(DcMotorEx.RunMode.RUN_TO_POSITION);
-        leftBack.setMode(DcMotorEx.RunMode.RUN_TO_POSITION);
-        rightBack.setMode(DcMotorEx.RunMode.RUN_TO_POSITION);
-    }
 
 }
