2e7842f1e36ff41a0b4bdd68a509aa97ed0684cc
ishaandesai <IshaanDesai21@gmail.com>
2025-09-24 21:52:38 -0500

    Remove OOP Ishaan

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Encoder_Drive/EncoderAutonTemplate.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Encoder_Drive/EncoderAutonTemplate.java
@@ -19,7 +19,40 @@ public class EncoderAutonTemplate extends LinearOpMode {
 
     @Override
     public void runOpMode() {
-        DrivetrainHardware drivehw = new DrivetrainHardware(hardwareMap);
+        leftFront = hardwareMap.get(DcMotorEx.class, "leftFront");
+        rightFront = hardwareMap.get(DcMotorEx.class, "rightFront");
+        leftBack = hardwareMap.get(DcMotorEx.class, "leftBack");
+        rightBack = hardwareMap.get(DcMotorEx.class, "rightBack");
+
+        //Resets all encoder values to 0
+        leftFront.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
+        rightFront.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
+        leftBack.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
+        rightBack.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
+
+        //Sets motor target positions to 0
+        leftFront.setTargetPosition(0);
+        rightFront.setTargetPosition(0);
+        leftBack.setTargetPosition(0);
+        rightBack.setTargetPosition(0);
+
+        //Set RunMode to Run to Position
+        leftFront.setMode(DcMotorEx.RunMode.RUN_TO_POSITION);
+        rightFront.setMode(DcMotorEx.RunMode.RUN_TO_POSITION);
+        leftBack.setMode(DcMotorEx.RunMode.RUN_TO_POSITION);
+        rightBack.setMode(DcMotorEx.RunMode.RUN_TO_POSITION);
+
+        //When power of 0 is applied, it will brake, rather than glide with inertia
+        leftFront.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
+        rightFront.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
+        leftBack.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
+        rightBack.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
+
+        //TODO: Set motor directions here, when a positive power is applied, it should go forward
+        leftFront.setDirection(DcMotorEx.Direction.FORWARD);
+        rightFront.setDirection(DcMotorEx.Direction.REVERSE);
+        leftBack.setDirection(DcMotorEx.Direction.FORWARD);
+        rightBack.setDirection(DcMotorEx.Direction.REVERSE);
 
         //--------------------- INITIALIZE ---------------------\\
         intake = hardwareMap.get(DcMotorEx.class, "intake");
