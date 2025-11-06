7baaba24a866bb20da1b428e9a97cf38701ced9c
madhanp <eshaann.madhan@gmail.com>
2025-11-06 16:46:57 -0600

    added comment

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -20,26 +20,26 @@ public class MeepMeepTesting {
                 .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
                 .build();
 
-        myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(48, 48, Math.toRadians(50)))
+        myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(-48, 48, Math.toRadians(125)))
                 .strafeToConstantHeading(new Vector2d(-47,47))
                 .waitSeconds(1.5)
                 //1st 3 balls over
                 .strafeToLinearHeading(new Vector2d(-12,22),Math.toRadians(90),new TranslationalVelConstraint(70))
                 .strafeToConstantHeading(new Vector2d(-12,55),new TranslationalVelConstraint(20))
-                .strafeToLinearHeading(new Vector2d(-47,47),Math.toRadians(50),new TranslationalVelConstraint(70))
+                .strafeToLinearHeading(new Vector2d(-47,47),Math.toRadians(125),new TranslationalVelConstraint(70))
                 .waitSeconds(1.5)
                 //hi
                 //2nd 3 balls over
                 .strafeToLinearHeading(new Vector2d(15,22),Math.toRadians(90),new TranslationalVelConstraint(70))
                 .strafeToConstantHeading(new Vector2d(15,62),new TranslationalVelConstraint(20))
                 .strafeToConstantHeading(new Vector2d(15,50))
-                .strafeToLinearHeading(new Vector2d(-47,47),Math.toRadians(50),new TranslationalVelConstraint(70))
+                .strafeToLinearHeading(new Vector2d(-47,47),Math.toRadians(125),new TranslationalVelConstraint(70))
                 .waitSeconds(1.5)
                 //3rd 3 balls over
                 .strafeToLinearHeading(new Vector2d(35,22),Math.toRadians(90),new TranslationalVelConstraint(70))
                 .strafeToConstantHeading(new Vector2d(35,62),new TranslationalVelConstraint(20))
                 .strafeToConstantHeading(new Vector2d(35,50))
-                .strafeToLinearHeading(new Vector2d(-47,47),Math.toRadians(50),new TranslationalVelConstraint(70))
+                .strafeToLinearHeading(new Vector2d(-47,47),Math.toRadians(125),new TranslationalVelConstraint(70))
                 .waitSeconds(1.5)
                 //4th 3 balls over
 
@@ -57,16 +57,9 @@ public class MeepMeepTesting {
 
                 .build());
 
-        BufferedImage bg = null;
-        try {
-            bg = ImageIO.read(new File("MeepMeepTesting/src/main/java/com/example/meepmeeptesting/DECODE.png"));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        meepMeep.setBackground(bg)
+        meepMeep.setBackground(MeepMeep.Background.FIELD_DECODE_JUICE_DARK)
                 .setDarkMode(true)
                 .setBackgroundAlpha(0.95f)
-
                 .addEntity(myBot)
                 .start();
     }
